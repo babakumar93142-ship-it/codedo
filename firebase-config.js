4e8fd8805548d9f46743890066bba847e21a025f
babakumar93142-ship-it <babakumar93142@gmail.com>
2026-08-25 13:07:47 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// ===== CodeGuru — Firebase config =====
+export const firebaseConfig = {
+  apiKey: "AIzaSyAb-U-kwJdmR9hGwo0RGB7zFQaq6gnbJXY",
+  authDomain: "codeguru-c4179.firebaseapp.com",
+  projectId: "codeguru-c4179",
+  storageBucket: "codeguru-c4179.firebasestorage.app",
+  messagingSenderId: "645429806529",
+  appId: "1:645429806529:web:e1a0fe7bc532491f969b40"
+};
+
+export const ADMIN_EMAIL = "babakumar93142@gmail.com";
